5e19c099d8ae0cc89fc40b437b79b9c0dba31010
Manny <Manny.SystemsEngineer@gmail.com>
2017-11-09 07:14:22 +0000

    Added different SIDC codes for tactical symbols

--- src/run/Main.java
+++ src/run/Main.java
@@ -7,6 +7,7 @@ import gov.nasa.worldwind.Configuration;
 import gov.nasa.worldwind.avlist.AVKey;
 
 import javax.swing.*;
+import java.util.Objects;
 import java.util.Scanner;
 
 import static gov.nasa.worldwindx.examples.ApplicationTemplate.start;
@@ -14,12 +15,18 @@ import static gov.nasa.worldwindx.examples.ApplicationTemplate.start;
 
 public class Main {
     public static void main(String[] args) {
-
+        String password = "manny";
         Scanner str_reader = new Scanner(System.in);
         System.out.println(("Login:"));
         String user = str_reader.next();
 
+        if(!Objects.equals(user, password)){
+            System.out.println("Access Denied!");
+            System.exit(0);
+        }
+
         System.out.println("Hello " + user + "!");
+
 
         //PduSender Sender = new PduSender(80, "TEST");
         //Sender.run();
--- src/run/TacticalSymbols.java
+++ src/run/TacticalSymbols.java
@@ -34,13 +34,42 @@ public class TacticalSymbols extends ApplicationTemplate {
             this.sharedHighlightAttrs.setTextModifierMaterial(Material.WHITE);
             this.sharedHighlightAttrs.setOpacity(1.0);
 
-            TacticalSymbol airSymbol = new MilStd2525TacticalSymbol("SFAPMFQM--GIUSA", Position.fromDegrees(32.4520, 63.44553, 3000));
+            TacticalSymbol airSymbol = new MilStd2525TacticalSymbol("SFAPMFQM-------", Position.fromDegrees(32.4520, 63.44553, 3000));
             airSymbol.setValue(AVKey.DISPLAY_NAME, "MIL-STD-2525 Friendly SOF Drone Aircraft");
             airSymbol.setAttributes(this.sharedAttrs);
             airSymbol.setHighlightAttributes(this.sharedHighlightAttrs);
             airSymbol.setModifier(SymbologyConstants.DIRECTION_OF_MOVEMENT, Angle.fromDegrees(235));
             airSymbol.setShowLocation(false);
             this.symbolLayer.addRenderable(airSymbol);
+
+            TacticalSymbol targetSymbol = new MilStd2525TacticalSymbol("SHGPIB----A----", Position.fromDegrees(32.4014, 63.3894, 0));
+            targetSymbol.setValue(AVKey.DISPLAY_NAME, "MIL-STD-2525 Hostile Facility");
+            targetSymbol.setAttributes(this.sharedAttrs);
+            targetSymbol.setHighlightAttributes(this.sharedHighlightAttrs);
+            targetSymbol.setModifier(SymbologyConstants.DIRECTION_OF_MOVEMENT, Angle.fromDegrees(90));
+            targetSymbol.setModifier(SymbologyConstants.SPEED_LEADER_SCALE, 0.5);
+            targetSymbol.setShowLocation(false);
+            this.symbolLayer.addRenderable(targetSymbol);
+
+            TacticalSymbol groundSymbol = new MilStd2525TacticalSymbol("SFGPUCI----E---", Position.fromDegrees(32.423, 63.324, 0));
+            groundSymbol.setValue(AVKey.DISPLAY_NAME, "MIL-STD-2525 Friendly Infantry Company");
+            groundSymbol.setAttributes(this.sharedAttrs);
+            groundSymbol.setHighlightAttributes(this.sharedHighlightAttrs);
+            groundSymbol.setModifier(SymbologyConstants.DIRECTION_OF_MOVEMENT, Angle.fromDegrees(90));
+            groundSymbol.setModifier(SymbologyConstants.SPEED_LEADER_SCALE, 0.5);
+            groundSymbol.setShowLocation(false);
+            this.symbolLayer.addRenderable(groundSymbol);
+
+            TacticalSymbol reconSymbol = new MilStd2525TacticalSymbol("SFGPUCR----B---", Position.fromDegrees(32.413, 63.40, 0));
+            reconSymbol.setValue(AVKey.DISPLAY_NAME, "MIL-STD-2525 Friendly Recon Squad");
+            reconSymbol.setAttributes(this.sharedAttrs);
+            reconSymbol.setHighlightAttributes(this.sharedHighlightAttrs);
+            reconSymbol.setModifier(SymbologyConstants.DIRECTION_OF_MOVEMENT, Angle.fromDegrees(90));
+            reconSymbol.setModifier(SymbologyConstants.SPEED_LEADER_SCALE, 0.5);
+            reconSymbol.setShowLocation(false);
+            this.symbolLayer.addRenderable(reconSymbol);
+
+
 
             this.getWwd().getModel().getLayers().add(symbolLayer);
 
